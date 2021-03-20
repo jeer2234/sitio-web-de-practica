832da1968dd680e867adff9201016dff149af078
Javier Espinoza <jeer2234@gmail.com>
2021-03-20 18:02:13 -0400

    creando formato base de pagina web mas paginas  de pruebas

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'homepage'
 
 urlpatterns = [
-    path('', views.visualizar, name= 'visualizar')
+
+    path('', views.index, name= 'index'),
+    path('about', views.about, name= 'about'),
+    path('contact', views.contact, name= 'contact'),
+    path('services', views.services, name= 'services'),
+
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,14 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .forms import NameForm
+
 
 # Create your views here.
 
 
 
-def renderizar(request):
-    context = {}
+def about(request):
     
-    return HttpResponse("Hello, world. You're at the polls index.")
-    
-def visualizar(request):
-    return render(request,'homepage/index.html',{})
+    return render(request,'homepage/about.html',)
+
+
+def index(request):
+    return render(request,'homepage/index.html',)
+
+
+def register(request):
+    """The login page for condimentos."""
+    return render(request,'homepage/register.html')
+
+def contact(request):
+    """The login page for condimentos."""
+    return render(request,'homepage/contact.html')
+
+def login(request):
+    """The login page for condimentos."""
+        # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request,'homepage/login.html', {'form': form})
+
+def services(request):
+    """The login page for condimentos."""
+    return render(request,'homepage/services.html')
